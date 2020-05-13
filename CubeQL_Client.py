5efe1479990431d426c0049ba0a46fae8ffa1393
xcloudfance <15906005579@139.com>
2020-05-13 10:52:36 +0800

    加了个CDS(分布式定制爬虫)

---
+++ CubeQL_Client.py
@@ -0,0 +1,23 @@
+from requests_html import requests
+import demjson
+class CubeQL:
+    host = '127.0.0.1'
+    port = '1278'
+    
+    def __init__(self):
+        pass
+    def get(self):
+        #print(self.host,self.port)
+        req = requests.post('http://'+self.host+':'+self.port+'/get')
+
+        return req.text
+    def set(self,url):
+        params = {'url':url}
+        hea = {'accept':'application/json'}
+        req = requests.post('http://'+self.host+':'+self.port+'/set?url='+url,hea)
+
+    def del_all(self):
+        pass
+if __name__ == '__main__':
+    cube = CubeQL()
+    cube.set('http://www.baidu.com')
