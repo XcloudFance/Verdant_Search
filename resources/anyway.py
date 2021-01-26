bb8402bbce079d1319c4c320f606d5bbdeabcc04
xcloudfance <15906005579@139.com>
2021-01-26 22:08:45 +0800

    改了下markdown描述问题

--- resources/anyway.py
+++
@@ -1,8 +0,0 @@
-def sort_by_value(d):
-    items=d.items()
-    backitems=[[v[1],v[0]] for v in items]
-    backitems.sort()
-    return [ backitems[i][1] for i in range(0,len(backitems))]
-a = {1:2,0:1}
-print(a)
-print(sort_by_value(a))
