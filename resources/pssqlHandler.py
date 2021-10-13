12743949f15503c49c717984463ca304228de80f
xcloudfance <15906005579@139.com>
2021-10-13 18:50:45 +0800

    Update pssqlHandler.py

--- resources/pssqlHandler.py
+++ resources/pssqlHandler.py
@@ -30,31 +30,47 @@ class pssql_Handler:
             
             # print(1)
             try:
-                self.cursor.execute("")
+                pass
+                #self.cursor.execute(';')
             except:
                 print('pssql error!')
             return func(*args,**kwargs)
         return regular_checks
 
     def increaseURLWeight(self,website): #给对应的url权值+1
-        self.cursor.execute("update content set weigh = weigh + 1 where url = %s", (website,))
-        self.pssql.commit()
+
+        try:
+            self.cursor.execute("update content set weigh = weigh + 1 where url = %s", (website,))
+            self.pssql.commit()
+        except:
+            self.pssql.rollback()
 
     def increaseKeywordWeight(self,keyword):#给对应的关键词权值+1
-        self.cursor.execute(
-            "update search set weigh = weigh + 1 where keyer = %s", (keyword,)
-        )
-        self.pssql.commit()
+        try:
+            self.cursor.execute(
+                "update search set weigh = weigh + 1 where keyer = %s", (keyword,)
+            )
+            self.pssql.commit()
+        except:
+            self.pssql.rollback()
     def recordLog(self,content,nowdate):#记录每天的dailylog,记载访问记录
-        self.cursor.execute("insert into daily_logs values('0',%s,%s);",(content,nowdate))
-        self.pssql.commit()
+        try:
+            self.cursor.execute("insert into daily_logs values('0',%s,%s);",(content,nowdate))
+            self.pssql.commit()
+        except:
+            self.pssql.rollback()
 
     def queryKeywordDescendingSort(self,keyword):#逆序排联想关键词
-        self.cursor.execute(
-            "select keyer from search where keyer like %s order by weigh desc",
-            (keyword + "%",),
-        )
-        return self.cursor.fetchall()
+        try:
+            self.cursor.execute(
+                "select keyer from search where keyer like %s order by weigh desc",
+                (keyword + "%",),
+            )
+            return self.cursor.fetchall()
+        except:
+            self.pssql.rollback()
+            return ""
+            
     def queryKeyword(self,keyword):#查询关键词
         try:
             self.cursor.execute('select value from search where keyer ~* %s;', (keyword,))
@@ -70,23 +86,35 @@ class pssql_Handler:
             return ""
         return self.cursor.fetchall()
     def getKeywordWeight(self,keyword):#获得权值
-        self.cursor.execute("select weigh from content where id = " + keyword)
+        try:
+            self.cursor.execute("select weigh from content where id = " + keyword)
+        except:
+            self.pssql.rollback()
+            return ""
         return self.cursor.fetchone()
     def getRecordDetails(self,id):
-        self.cursor.execute("select * from content where id = " + id)
+        try:
+            self.cursor.execute("select * from content where id = " + id)
+        except:
+            self.pssql.rollback()
+            return ""
         return self.cursor.fetchone()
 
     def getKeywordTrend(self,keyword):#这边是青荇趋势的获取日期和对应的搜索权值
-        self.cursor.execute("select timerange from daily_logs where content = %s",(keyword,))
-        res = self.cursor.fetchall()
-        ret = []
-        tmp_dict = {}
-        for i in res:
-            date = str(i[0].year) + "-"+str(i[0].month)+ "-"+str(i[0].day)
-            if date in tmp_dict:
-                tmp_dict[date] += 1
-            else:
-                tmp_dict[date] = 1 
-        for j in tmp_dict:
-            ret.append([j,tmp_dict[j]])
-        return ret
+        try:
+            self.cursor.execute("select timerange from daily_logs where content = %s",(keyword,))
+            res = self.cursor.fetchall()
+            ret = []
+            tmp_dict = {}
+            for i in res:
+                date = str(i[0].year) + "-"+str(i[0].month)+ "-"+str(i[0].day)
+                if date in tmp_dict:
+                    tmp_dict[date] += 1
+                else:
+                    tmp_dict[date] = 1 
+            for j in tmp_dict:
+                ret.append([j,tmp_dict[j]])
+            return ret
+        except:
+            self.pssql.rollback()
+            return ""
